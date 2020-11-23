28b422cf1fa062a2ed7f2cb9f693f9cd2b22076e
timyshark <61923560+timyshark@users.noreply.github.com>
2020-11-23 12:19:40 -0800

    Add create test user to UserSeeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -17,11 +17,11 @@ class UsersTableSeeder extends Seeder
         //
         User::truncate();
         \App\Models\User::factory(10)->create();
-        // User::create([
-        //     'name' => 'timyshark',
-        //     'email' => 'timy.shark@outlook.com',
-        //     'password' => app('hash')->make('secret')
-        // ]);
+        User::create([
+            'name' => 'timyshark',
+            'email' => 'timy.shark@outlook.com',
+            'password' => app('hash')->make('secret')
+        ]);
         // for ($i=0; $i< 10; $i++){
         //     User::create([
         //         'name' => 'user' . $i,
